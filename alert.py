757d50251d7d9d5348feac7497ce0b830deb5c87
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2023-08-01 11:35:13 +0900

    added daily log

--- alert.py
+++ alert.py
@@ -16,9 +16,8 @@ def get_name_point(text):
     return [text.split()[0], int(text.split()[-2].replace(",", ""))]
 
 
-def update_balance_log(balance, today, n_latest=365):
+def update_balance_log(balance, today):
     df = pd.read_csv(BALANCE_LOG_PATH)
-    df = df.iloc[-(n_latest - 1) :]
     new_row = {
         "Date": today,
         "Balance": balance,
@@ -27,15 +26,17 @@ def update_balance_log(balance, today, n_latest=365):
     new_df.to_csv(BALANCE_LOG_PATH, index=False)
 
 
-def update_userwise_log(charge_info, today, n_latest=93):
+def update_userwise_log(charge_info, today):
     user_point_dict = {"User": [], "Point": []}
     for i in range(5, len(charge_info) - 1):
         name, point = get_name_point(charge_info[i])
         user_point_dict["User"].append(name)
         user_point_dict["Point"].append(point)
     df = pd.DataFrame(user_point_dict)
+    """
     for old_data in sorted(Path(USERWISE_DIR).glob("*.csv"))[:-n_latest]:
         old_data.unlink()
+    """
     df.to_csv(f"{USERWISE_DIR}/{today}.csv", index=False)
 
 
@@ -54,13 +55,17 @@ if __name__ == "__main__":
     # get balance
     balance = get_name_point(charge_info[2])[-1]
     # Alert by Slack
+    client = WebhookClient(SLACK_WEBHOOK_URL_FURO)
     if balance < args.balance:
-        # channel
-        client = WebhookClient(SLACK_WEBHOOK_URL_FURO)
+        # alert
         message = "<!channel>\n"
         message += ":warning: *There are few points left.*\n"
         message += f"Balance: {balance:18} p"
-        response = client.send(text=message)
+    else:
+        # daily log
+        client = WebhookClient(SLACK_WEBHOOK_URL_FURO)
+        message = f"Balance: {balance:18} p"
+    response = client.send(text=message)
 
     update_balance_log(balance, today)
     update_userwise_log(charge_info, today)

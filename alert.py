19fe2e465cd4aadab871e300c32502afb150a74d
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2024-01-01 13:44:49 +0900

    added alert for user usage

--- alert.py
+++ alert.py
@@ -1,10 +1,12 @@
 import argparse
 import datetime
+import json
 import os
 import subprocess
 from pathlib import Path
 
 import pandas as pd
+from dateutil.relativedelta import relativedelta
 from slack_sdk import WebhookClient
 
 SLACK_WEBHOOK_URL_FURO = os.environ["SLACK_WEBHOOK_URL_FURO"]
@@ -14,6 +16,10 @@ USERWISE_DIR = "data/userwise"
 
 def get_name_point(text):
     return [text.split()[0], int(text.split()[-2].replace(",", ""))]
+
+
+def to_dict(df):
+    return dict(zip(df["User"], df["Point"]))
 
 
 def update_balance_log(balance, today, filepath):
@@ -40,8 +46,71 @@ def update_userwise_log(charge_info, filepath):
     df.to_csv(filepath, index=False)
 
 
-def to_dict(df):
-    return dict(zip(df["User"], df["Point"]))
+def get_user_diff(today, months, days, prefix=""):
+    ### Get info
+    user_diff_dict = {}
+    with open("data/userdict.json", "r") as f:
+        userdict = json.load(f)
+    lastday = today - relativedelta(months=months, days=days)
+    dict_new = to_dict(pd.read_csv(f"{USERWISE_DIR}/{today}{prefix}.csv"))
+    dict_old = to_dict(pd.read_csv(f"{USERWISE_DIR}/{lastday}{prefix}.csv"))
+    total_usage = 0
+    for user_id, user_pt in dict_new.items():
+        if user_pt == 0:
+            continue
+        # point
+        if user_id in dict_old:
+            diff_pt = user_pt - dict_old[user_id]
+        else:
+            diff_pt = user_pt  # new member
+        total_usage += diff_pt
+        if diff_pt == 0:
+            continue
+        # user name
+        if user_id in userdict:
+            user = userdict[user_id]
+        else:
+            user = user_id
+        # store into dict
+        user_diff_dict[user] = diff_pt
+    return total_usage, user_diff_dict, lastday
+
+
+def alert_user(user_diff_dict, user_threshold):
+    is_big_difference = False
+    message = (
+        "Big difference:\n"  # <!subteam^{SERVERADMIN_ID}> Please contact the user.\n"
+    )
+    userwise_message = "```"
+    for user, diff_pt in user_diff_dict.items():
+        if diff_pt > user_threshold:
+            is_big_difference = True
+            userwise_message += f"{user}: {diff_pt:,} p, "
+    userwise_message += "```"
+    message += userwise_message
+
+    if is_big_difference:
+        return message
+    else:
+        return ""
+
+
+def message_balance(balance, balance_threshold):
+    if balance < balance_threshold:
+        message = "<!channel>\n"
+        message += ":warning: *There are few points left.*\n"
+        message += f"Balance: {balance:18,} p\n"
+    else:
+        message = f"Balance: {balance:18,} p\n"
+    return message
+
+
+def message_user(user_diff_dict):
+    userwise_message = "```"  # code block
+    for user, diff_pt in user_diff_dict.items():
+        userwise_message += f"{user}: {diff_pt:,} pt\n"
+    userwise_message += "```"  # code block
+    return userwise_message
 
 
 def main(args):
@@ -59,67 +128,35 @@ def main(args):
         update_balance_log(balance, today, BALANCE_LOG_PATH + args.prefix + ".csv")
     if args.userwise_log:
         update_userwise_log(charge_info, f"{USERWISE_DIR}/{today}{args.prefix}.csv")
+    ## Get user_diff_dict
+    if args.announce_type == "daily":
+        total_usage, user_diff_dict, lastday = get_user_diff(today, months=0, days=1)
+    elif args.announce_type == "monthly":
+        total_usage, user_diff_dict, lastday = get_user_diff(
+            today, months=1, days=0, prefix=args.prefix
+        )
 
     # Announce via slack ###############################################
-    ## Daily
-    if args.daily_announce:
-        client = WebhookClient(SLACK_WEBHOOK_URL_FURO)
-        if balance < args.balance:
-            message = "<!channel>\n"
-            message += ":warning: *There are few points left.*\n"
-            message += f"Balance: {balance:18,} p"
-        else:
-            client = WebhookClient(SLACK_WEBHOOK_URL_FURO)
-            message = f"Balance: {balance:18,} p"
-        response = client.send(text=message)
-    ## Monthly
-    if args.monthly_announce:
-        client = WebhookClient(SLACK_WEBHOOK_URL_FURO)
-        import json
-
-        import pandas as pd
-        from dateutil.relativedelta import relativedelta
-
-        ### Get info
-        with open("data/userdict.json", "r") as f:
-            userdict = json.load(f)
-        lastmonth = today - relativedelta(months=1)
-        dict_new = to_dict(pd.read_csv(f"{USERWISE_DIR}/{today}{args.prefix}.csv"))
-        dict_old = to_dict(pd.read_csv(f"{USERWISE_DIR}/{lastmonth}{args.prefix}.csv"))
-        total_usage = 0
-        userwise_message = "```"  # code block
-        for user_id, user_pt in dict_new.items():
-            if user_pt == 0:
-                continue
-            # point
-            if user_id in dict_old:
-                diff_pt = user_pt - dict_old[user_id]
-            else:
-                diff_pt = user_pt
-            total_usage += diff_pt
-            if diff_pt == 0:
-                continue
-            # user name
-            if user_id in userdict:
-                user = userdict[user_id]
-            else:
-                user = user_id
-            userwise_message += f"{user}: {diff_pt:,} pt\n"
-        userwise_message += "```"  # code block
-        ### Announce
-        message = f":owl:*Monthly Usage ({lastmonth} to {today})*\n"
+    client = WebhookClient(SLACK_WEBHOOK_URL_FURO)
+    if args.announce_type == "daily":
+        message = message_balance(balance, args.threshold)
+        message += "\n"
+        message += alert_user(user_diff_dict, args.user_threshold)
+    elif args.announce_type == "monthly":
+        message = f":owl:*Monthly Usage ({lastday} to {today})*\n"
         message += f"*Total usage: {total_usage:18,} p*\n"
         message += f"*Userwise usage:*\n"
         message += "<https://docs.google.com/spreadsheets/d/1_wLEEGqrHAu2SMJfbTG5mmiXAe8P9kBwfxMQ2eWnYIo/edit?resourcekey#gid=32992617|Please report your usage.>\n"
-        message += userwise_message
-        response = client.send(text=message)
+        message += message_user(user_diff_dict)
+
+    response = client.send(text=message)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--balance", type=int, default=5000)
-    parser.add_argument("--daily_announce", action="store_true")
-    parser.add_argument("--monthly_announce", action="store_true")
+    parser.add_argument("--threshold", type=int, default=10000)
+    parser.add_argument("--user_threshold", type=int, default=10000)
+    parser.add_argument("--announce_type", type=str)
     parser.add_argument("--balance_log", action="store_true")
     parser.add_argument("--userwise_log", action="store_true")
     parser.add_argument("--prefix", type=str, default="")

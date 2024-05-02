57ab974958f3e965bc5ce06a110c5b9f69c420bf
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2024-05-02 10:55:46 +0900

    added STOP command to sinfo

--- sinfo.py
+++ sinfo.py
@@ -19,14 +19,15 @@ def main(args):
     cmd = ["ssh", "lab-mrcompute01", "sinfo -R"]
     result = subprocess.run(cmd, capture_output=True, text=True)
     if len(result.stdout.splitlines()) > 1:
-        client = WebhookClient(SLACK_WEBHOOK_URL_SLURM)
-        message = f":warning: <!subteam^{SERVERADMIN_ID}> Slurm is not normal. Please follow <https://www.notion.so/todalab/0c6b0040a41844408fea8b4b89c7b3e8?v=86497d48b35d43e2ac97849c49963c26&pvs=4|this instructions.>\n"
-        message += str(now) + "\n"
-        message += "Result of sinfo -R\n"
-        message += "```"
-        message += result.stdout
-        message += "```"
-        response = client.send(text=message)
+        if any(["STOP" not in x for x in result.stdout.splitlines()[1:]]):
+            client = WebhookClient(SLACK_WEBHOOK_URL_SLURM)
+            message = f":warning: <!subteam^{SERVERADMIN_ID}> Slurm is not normal. Please follow <https://www.notion.so/todalab/0c6b0040a41844408fea8b4b89c7b3e8?v=86497d48b35d43e2ac97849c49963c26&pvs=4|this instructions.>\n"
+            message += str(now) + "\n"
+            message += "Result of sinfo -R\n"
+            message += "```"
+            message += result.stdout
+            message += "```"
+            response = client.send(text=message)
 
 
 if __name__ == "__main__":
